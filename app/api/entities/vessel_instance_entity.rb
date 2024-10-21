c11d43914d7167d84ca99bec8f4c2f9ae97684fc
Ashwini <amurthy.ashwini@gmail.com>
2024-10-21 14:16:34 +0000

    wip: basic get and post api for vessel

---
+++ app/api/entities/vessel_instance_entity.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module Entities
+  class VesselInstanceEntity < Grape::Entity
+    expose :id
+    expose :name
+    expose :short_label
+    expose :description
+    expose :vessel_template
+    expose :bar_code
+    expose :qr_code
+    expose :tag
+    expose :container, using: 'Entities::ContainerEntity'
+  end
+end
